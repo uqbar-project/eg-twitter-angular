c91c232a22166bb2a41d1770f0456aafa97fba11
Fernando Dodino <fernando.dodino@gmail.com>
2021-07-19 21:42:02 -0300

    Adding tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,8 @@
-import { waitForAsync, TestBed } from '@angular/core/testing'
+import { waitForAsync, TestBed, ComponentFixture } from '@angular/core/testing'
 
 import { AppComponent } from './app.component'
+
+let fixture: ComponentFixture<AppComponent>
 
 describe('AppComponent', () => {
   beforeEach(waitForAsync(() => {
@@ -9,12 +11,38 @@ describe('AppComponent', () => {
         AppComponent
       ],
     }).compileComponents()
+    fixture = TestBed.createComponent(AppComponent)
+    fixture.detectChanges()
   }))
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent)
-    const app = fixture.componentInstance
-    expect(app).toBeTruthy()
-  })
+  it('should decrease letters - greater than 0', waitForAsync(() => {
+    twittear('En todos lados se cuecen habas')
+    fixture.detectChanges()
+    fixture.whenStable().then(() => {
+      expect(buscarElemento('restantes').textContent).toContain('110')
+    })
+  }))
 
+  it('should decrease letters - exactly 0', waitForAsync(() => {
+    twittear('12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890')
+    fixture.detectChanges()
+    fixture.whenStable().then(() => {
+      expect(buscarElemento('restantes').textContent).toContain('0')
+      expect(fixture.componentInstance.twitter.excedido).toBeTruthy()
+    })
+  }))
+
+  /* Función auxiliar que permite buscar un elemento por data-testid */
+  function buscarElemento(testId: string) {
+    const compiled = fixture.debugElement.nativeElement
+    return compiled.querySelector(`[data-testid="${testId}"]`)
+  }
+
+  function twittear(tweet: string) {
+    const twitter = fixture.componentInstance.twitter
+    twitter.texto = tweet
+    const inputTexto = buscarElemento('texto')
+    inputTexto.value = twitter.texto    
+    return twitter
+  }
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 import { FormsModule } from '@angular/forms'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
 
 @NgModule({
   declarations: [
